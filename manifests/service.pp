366dd34b759980dc9fdd18189b45f38dead9c32a
Pablo Carranza <pablo@vdevices.com>
2013-10-04 16:41:34 -0500

    Create service.pp

---
+++ manifests/service.pp
@@ -0,0 +1,13 @@
+class sogo::service {
+  service { 'sogo':
+    ensure      => running,
+    enable      => true,
+    hasstatus   => true,
+    hasrestart  => true,
+  }
+
+  cron { 'sogo-ealarms-notify':
+    command => '/usr/sbin/sogo-ealarms-notify',
+    user    => 'sogo',
+  }
+}
